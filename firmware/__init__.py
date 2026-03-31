1c8fc13fdbe4eb480aae55bd4047a1f68a6abecd
3D-uy <136130959+3D-uy@users.noreply.github.com>
2026-03-31 16:30:24 -0300

    Update __init__.py

--- firmware/__init__.py
+++ firmware/__init__.py
@@ -1,4 +1,6 @@
 # Firmware module for KACE
 from .generator import generate_firmware_config
-from .builder import build_firmware
-from .mcu_map import MCU_CONFIGS
+from .builder import build_firmware_orchestrator
+from .derivation import derive_config
+from .detector import discover_mcu_hardware
+from .validator import validate_config
