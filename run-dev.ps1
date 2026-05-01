cab8eff7fb9a8e31b6d7447e057798d6da03979d
3D-uy <136130959+3D-uy@users.noreply.github.com>
2026-05-01 19:15:41 -0300

    chore: untrack dev scripts and test results from repository

--- run-dev.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Running KACE in DEV mode 😈"
-
-docker compose -f docker-compose.dev.yml build --no-cache
-docker compose -f docker-compose.dev.yml run --rm kace
--- run-prod.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Running KACE in PROD mode 🚀"
-
-docker compose build --no-cache
-docker compose run --rm kace
